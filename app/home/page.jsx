9c56f0444d939e05405ccdb2d98c2e6d302f3689
Jakub Lewandowski <kubalew.pl@gmail.com>
2023-11-08 11:21:35 +0100

    added platforms and access token refresh flow

--- app/home/page.jsx
+++ app/home/page.jsx
@@ -1,8 +1,10 @@
 import PrivateRoute from "@/components/PrivateRoute";
-import { getServerSession } from "next-auth";
 import { OPTIONS } from "../api/auth/[...nextauth]/route";
+import Platform from "@/components/Platform";
+import { getServerSession } from "next-auth";
+import GenrePiechart from "@/components/GenrePiechart";
 
-const HomePage = async () => {
+const HomePage = () => {
   return <PrivateRoute>{<HomePageContent />}</PrivateRoute>;
 };
 
@@ -11,14 +13,22 @@ const HomePageContent = async () => {
 
   const fetchTop = async (type) => {
     // type == 'tracks' or 'artists'
-    const response = await fetch(`https://api.spotify.com/v1/me/top/${type}`, {
-      headers: {
-        Authorization: `Bearer ${session.accessToken}`,
-        cache: "no-cache",
-      },
-    });
-    const data = await response.json();
-    return await data.items;
+    try {
+      const response = await fetch(
+        `https://api.spotify.com/v1/me/top/${type}`,
+        {
+          headers: {
+            Authorization: `Bearer ${session.accessToken}`,
+            cache: "no-cache",
+          },
+        }
+      );
+      const resData = await response.json();
+
+      return resData.items;
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   const topArtists = await fetchTop("artists");
@@ -27,28 +37,13 @@ const HomePageContent = async () => {
   return (
     <div className="w-full flex-1 flex flex-col items-center bg-primary">
       <div className="w-full h-full grid grid-cols-2 grid-rows-5 gap-4 p-4">
-        {/* <div className="w-full bg-neutral-800 flex flex-col row-span-2 gap-y-2 items-center justify-center px-4 rounded-lg text-white hover:cursor-pointer hover:outline hover:outline-2 hover:outline-offset-4 hover:outline-secondary duration-150">
-          {topArtists.slice(0, 5).map((artist, i) => (
-            <div key={artist.name} className="w-full text-3xl">
-              <span>{i + 1} - </span>
-              <span className="">{artist.name}</span>
-            </div>
-          ))}
-        </div> */}
-        <div className="w-full bg-neutral-800 flex flex-col row-span-2 gap-y-2 items-center justify-center px-4 rounded-lg text-white hover:cursor-pointer hover:outline hover:outline-2 hover:outline-offset-4 hover:outline-secondary duration-150">
-          <div className="w-12 bg-secondary min-h-0 max-h-[50%] duration-150 animate-slideUp delay-400"></div>
-          <div className="w-12 bg-secondary min-h-0 max-h-[70%] duration-150 animate-slideUp delay-600"></div>
-          <div className="w-12 bg-secondary min-h-0 max-h-[30%] duration-150 animate-slideUp delay-200"></div>
+        <Platform type="artists" data={topArtists.slice(0, 3) || []} />
+        <Platform type="tracks" data={topTracks.slice(0, 3) || []} />
+        <div className="h-full w-full col-span-2 row-span-3 bg-neutral-800 hover:outline hover:outline-2 hover:outline-offset-4 hover:outline-secondary duration-150">
+          <div className="p-4 w-full h-full">
+            <GenrePiechart />
+          </div>
         </div>
-        <div className="w-full bg-neutral-800 flex flex-col row-span-2   gap-y-2 items-center justify-center px-4 rounded-lg text-white hover:cursor-pointer hover:outline hover:outline-2 hover:outline-offset-4 hover:outline-secondary duration-150">
-          {topTracks.slice(0, 5).map((track, i) => (
-            <div key={track.name} className="w-full text-3xl">
-              <span>{i + 1} - </span>
-              <span className="">{track.name}</span>
-            </div>
-          ))}
-        </div>
-        <div className="h-full w-full col-span-2 row-span-3 bg-neutral-800 hover:outline hover:outline-2 hover:outline-offset-4 hover:outline-secondary duration-150"></div>
       </div>
     </div>
   );
