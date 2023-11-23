9c314d8b6aeb4641405b9a2bb8652bc7e537d4e2
Jakub Lewandowski <kubalew.pl@gmail.com>
2023-11-23 14:47:52 +0100

    fixed profile picture issues

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -3,6 +3,7 @@ import PrivateRoute from "@/components/PrivateRoute";
 import { useSession } from "next-auth/react";
 import { getServerSession } from "next-auth";
 import { OPTIONS } from "../api/auth/[...nextauth]/route";
+import { FaUser } from "react-icons/fa";
 
 const ProfilePage = async () => {
   return <PrivateRoute>{<ProfilePageContent />}</PrivateRoute>;
@@ -17,23 +18,23 @@ const ProfilePageContent = async () => {
     },
   });
 
-  const data = await response.json();
+  const data = response.ok ? await response.json() : [];
 
   return (
     <div className="w-full h-full bg-primary flex flex-col items-center pt-12 text-white gap-y-4">
       <div className="overflow-hidden h-64 w-64 rounded-full flex items-center justify-center">
-        <img
-          alt="profile picture"
-          src={
-            data.images[1].url ||
-            data.images[0].url ||
-            "https://static.vecteezy.com/system/resources/previews/008/442/086/non_2x/illustration-of-human-icon-user-symbol-icon-modern-design-on-blank-background-free-vector.jpg"
-          }
-          className="w-full h-full"
-        />
+        {response.ok ? (
+          <img
+            alt="profile picture"
+            src={data.images[1].url || data.images[0].url}
+            className="w-full h-full"
+          />
+        ) : (
+          <FaUser className="w-full h-full" />
+        )}
       </div>
       <Link
-        href={data.external_urls.spotify}
+        href={response.ok ? data.external_urls.spotify : "#"}
         className="text-3xl font-semibold underline underline-offset-8 hover:text-secondary hover:cursor-pointer transition-colors duration-150"
       >
         {!data.display_name ? "Logged User" : data.display_name}
